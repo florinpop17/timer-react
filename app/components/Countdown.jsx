7f7414244f0fd69f1822c002aa8d237336bd26af
Florin Pop <popflorin1705@yahoo.com>
2016-12-16 12:06:30 +0200

    style clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
     render: function() {
         return (
             <div>
-                <p>Countdown page</p>
+                <Clock totalSeconds={129} />
             </div>
         );
     }
