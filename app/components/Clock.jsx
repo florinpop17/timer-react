5428fcff67d0c14eb7121b8306dc62758fe8e032
Florin Pop <popflorin1705@yahoo.com>
2016-12-16 11:53:53 +0200

    checked if min/sec < 10 and added leading 0

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -5,6 +5,14 @@ var Clock = React.createClass({
         var seconds = totalSeconds % 60;
         var minutes = Math.floor(totalSeconds / 60);
         
+        if(seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        
+        if(minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        
         return minutes + ':' + seconds;
     },
     
