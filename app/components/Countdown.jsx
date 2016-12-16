1254ab5a994de289387fc507b3fd232c31424508
Florin Pop <popflorin1705@yahoo.com>
2016-12-16 12:18:43 +0200

    added formSubmision functionality

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,25 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
+    getInitialState: function() {
+        return {count:0}
+    },
+    
+    handleSetCountdown: function(seconds) {
+        this.setState({
+            count: seconds
+        });
+    },
+    
     render: function() {
+        var {count} = this.state;
+        
         return (
             <div>
-                <Clock totalSeconds={129} />
+                <Clock totalSeconds={count} />
+                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
             Nav: 'app/components/Nav.jsx',
             Timer: 'app/components/Timer.jsx',
             Countdown: 'app/components/Countdown.jsx',
-            Clock: 'app/components/Clock.jsx'
+            Clock: 'app/components/Clock.jsx',
+            CountdownForm: 'app/components/CountdownForm.jsx'
         },
         extensions: ['', '.js', '.jsx']
     },
