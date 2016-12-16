dd9676a18ecef63ca8fcd23766c6a7fe2a4bbdb5
Florin Pop <popflorin1705@yahoo.com>
2016-12-16 12:14:58 +0200

    addded onSubmit functionality

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 
 var CountdownForm = React.createClass({
+    onSubmit: function(e) {
+        e.preventDefault();
+        
+        var strSeconds = this.refs.seconds.value;
+        
+        if (strSeconds.match(/^[0-9]*$/)) {
+            this.refs.seconds.value = '';
+            
+            this.props.onSetCountdown(parseInt(strSeconds, 10));
+        }
+    },
+    
     render: function() {
         return (
             <div>
