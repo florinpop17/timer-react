bd177c3f033a7c59a7ae8a88ecf3f0bd317e070e
Florin Pop <popflorin1705@yahoo.com>
2016-12-16 12:01:33 +0200

    added more tests

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -11,6 +11,16 @@ describe('Clock', () => {
         expect(Clock).toExist(); 
     });
     
+    describe('render', () => {
+        it('should render clock to output', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62} />);
+            var $el = $(ReactDOM.findDOMNode(clock));
+            var actualText = $el.find('.clock-text').text();
+            
+            expect(actualText).toBe('01:02');
+        });
+    });
+    
     describe('formatSeconds', () => {
         it('should format seconds', () => {
             var clock = TestUtils.renderIntoDocument(<Clock />); 
