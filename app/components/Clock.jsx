8709cbef96d7d68cee4b85b78ccdece42993f19c
Florin Pop <popflorin1705@yahoo.com>
2016-12-16 11:48:08 +0200

    added formatSeconds function

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,6 +1,13 @@
 var React = require('react');
 
 var Clock = React.createClass({
+    formatSeconds: function(totalSeconds) {
+        var seconds = totalSeconds % 60;
+        var minutes = Math.floor(totalSeconds / 60);
+        
+        return minutes + ':' + seconds;
+    },
+    
     render: function() {
         return (
             <div>
