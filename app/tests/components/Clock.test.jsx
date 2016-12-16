1fab9c282c59e2c1a639efa394119066e9215e0e
Florin Pop <popflorin1705@yahoo.com>
2016-12-16 11:54:04 +0200

    added extra tests

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -10,4 +10,23 @@ describe('Clock', () => {
     it('should exist', () => {
         expect(Clock).toExist(); 
     });
+    
+    describe('formatSeconds', () => {
+        it('should format seconds', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock />); 
+            var seconds = 615;
+            var expected = '10:15';
+            var actual = clock.formatSeconds(seconds);
+            expect(actual).toBe(expected);
+        });
+        
+        
+        it('should format seconds when min/sec < 10', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock />); 
+            var seconds = 61;
+            var expected = '01:01';
+            var actual = clock.formatSeconds(seconds);
+            expect(actual).toBe(expected);
+        });
+    });
 });
