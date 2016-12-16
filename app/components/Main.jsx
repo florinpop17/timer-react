7f7079b0aaf88eb10513fd9cff00a94b40c8d78e
Florin Pop <popflorin1705@yahoo.com>
2016-12-16 12:19:58 +0200

    added foundation classes for responsiveness

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,10 +3,9 @@ var Nav = require('Nav');
 
 var Main = (props) => (
         <div>
-            <div>
-                <div>
-                    <Nav/>
-                    <p>Main.jsx rendered</p>
+            <Nav/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
                     {props.children}
                 </div>
             </div>
