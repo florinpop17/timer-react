8a692adf56f91c9a45148649752cd24745ec0aa4
Florin Pop <popflorin1705@yahoo.com>
2016-12-19 15:54:52 +0200

    added controls.jsx to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,9 @@ module.exports = {
             Timer: 'app/components/Timer.jsx',
             Countdown: 'app/components/Countdown.jsx',
             Clock: 'app/components/Clock.jsx',
-            CountdownForm: 'app/components/CountdownForm.jsx'
+            CountdownForm: 'app/components/CountdownForm.jsx',
+            Controls: 'app/components/Controls.jsx'
+            
         },
         extensions: ['', '.js', '.jsx']
     },
