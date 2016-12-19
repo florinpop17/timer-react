894ed46fd9efd5139ff61f3f56f0ad8291e489d3
Florin Pop <popflorin1705@yahoo.com>
2016-12-19 15:50:09 +0200

    added controls.jsx

---
+++ app/components/Controls.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Controls = React.createClass({
+    propTypes: {
+        countdownStatus: React.PropTypes.string.isRequired
+    }
+});
+
+module.exports = Controls.
