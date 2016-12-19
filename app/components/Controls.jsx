8426d8fc846772b8063d2ad76a83f722617de92c
Florin Pop <popflorin1705@yahoo.com>
2016-12-19 15:53:45 +0200

    added buttons start/pause/clear

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -3,7 +3,25 @@ var React = require('react');
 var Controls = React.createClass({
     propTypes: {
         countdownStatus: React.PropTypes.string.isRequired
+    },
+    
+    render: function() {
+        var {countdownStatus} = this.props;
+        var renderStartStopButton = () => {
+            if(countdownStatus === 'started') {
+                return <button className="button secondary">Pause</button>
+            } else if(countdownStatus === 'pause') {
+                return <button className="button primary">Start</button>
+            } 
+        };
+        
+        return (
+            <div className="controls">
+                {renderStartStopButton()}
+                <button className="button alert hollow">Clear</button>
+            </div>
+        )
     }
 });
 
-module.exports = Controls.
+module.exports = Controls;
