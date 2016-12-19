a850e4ef23e6a7fe640b293598eea093461c47dd
Florin Pop <popflorin1705@yahoo.com>
2016-12-19 15:33:21 +0200

    added test for CountdownForm

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -22,12 +22,12 @@ describe('CountdownForm', () => {
         expect(spy).toHaveBeenCalledWith(109);
     });
     
-    it('it should not call onSetCountdown if invalid seconds entered', () => {
+    it('should not call onSetCountdown if invalid seconds entered', () => {
         var spy = expect.createSpy();
         var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
         var $el = $(ReactDOM.findDOMNode(countdownForm));
         
-        countdownForm.refs.seconds.value = '109';
+        countdownForm.refs.seconds.value = '109b';
         TestUtils.Simulate.submit($el.find('form')[0]);
         
         expect(spy).toNotBeenCalledWith(109);
